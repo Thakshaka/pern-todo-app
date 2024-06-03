14a8ac32eb75b194b0054d0f1ba01eb92f1d742e
Thakshaka Rathnayake <thakshakarathnayake15@gmail.com>
2024-06-03 13:41:31 +0530

    Updates

---
+++ precache-manifest.79647252bbc23833ca0719a436713aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caef9bafd5b6152db32279425a42077f",
+    "url": "/pern-todo-app/index.html"
+  },
+  {
+    "revision": "72775759dedd4d0b94fb",
+    "url": "/pern-todo-app/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "23e8d6229f252de14e63",
+    "url": "/pern-todo-app/static/js/2.5c3cbdc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pern-todo-app/static/js/2.5c3cbdc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72775759dedd4d0b94fb",
+    "url": "/pern-todo-app/static/js/main.17e78b3e.chunk.js"
+  },
+  {
+    "revision": "ebc9e1a3ad94fb6033b3",
+    "url": "/pern-todo-app/static/js/runtime-main.66e94738.js"
+  }
+]);
